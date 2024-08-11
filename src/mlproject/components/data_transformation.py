b7ad2714d26dfedd56fb485de897c352a81f4ae8
Yours01amit <emmysingh019@gmail.com>
2024-08-11 23:26:14 +0530

    data transformation and model training

--- src/mlproject/components/data_transformation.py
+++ src/mlproject/components/data_transformation.py
@@ -1,8 +1,8 @@
 import sys
 from dataclasses import dataclass
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
@@ -18,16 +18,16 @@ import os
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
 
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config = DataTransformationConfig()
+        self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
-        """
+        '''
         this function is responsible for data transformation
-        """
+        '''
         try:
             numerical_columns = ["writing score", "reading score"]
             categorical_columns = [
@@ -39,19 +39,19 @@ class DataTransformation:
             ]
             num_pipeline=Pipeline(steps=[
                 ("imputer",SimpleImputer(strategy='median')),
-                ("scaler", StandardScaler())
+                ('scalar',StandardScaler())
 
             ])
             cat_pipeline=Pipeline(steps=[
-                ("imputer",SimpleImputer(strategy="most_frequent")),
-                ("one_hot_encoder", OneHotEncoder()),
-                ("scaler", StandardScaler(with_mean=False))
+            ("imputer",SimpleImputer(strategy="most_frequent")),
+            ("one_hot_encoder",OneHotEncoder()),
+            ("scaler",StandardScaler(with_mean=False))
             ])
 
-            logging.info(f"categorical columns:{categorical_columns}")
-            logging.info(f"numerical columns:{numerical_columns}")
+            logging.info(f"Categorical Columns:{categorical_columns}")
+            logging.info(f"Numerical Columns:{numerical_columns}")
 
-            preprocessor = ColumnTransformer(
+            preprocessor=ColumnTransformer(
                 [
                     ("num_pipeline",num_pipeline,numerical_columns),
                     ("cat_pipeline",cat_pipeline,categorical_columns)
@@ -59,53 +59,54 @@ class DataTransformation:
 
             )
             return preprocessor
-
+            
 
         except Exception as e:
             raise CustomException(e,sys)
         
-    def initiate_data_transformation(self,train_path,test_path):
+    def initiate_data_transormation(self,train_path,test_path):
         try:
-            train_df = pd.read_csv(train_path)
-            test_df  = pd.read_csv(test_path)
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
 
             logging.info("Reading the train and test file")
 
-            preprocessor_obj = self.get_data_transformer_object()
+            preprocessing_obj=self.get_data_transformer_object()
 
-            target_column_name = "math score"
+            target_column_name="math score"
             numerical_columns = ["writing score", "reading score"]
 
-            # divide the train dataset to independent and dependent feature
+            ## divide the train dataset to independent and dependent feature
 
-            input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
-            target_feature_train_df = train_df[target_column_name]
+            input_features_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
 
-            # divide the test dataset to independent and dependent feature
+            ## divide the test dataset to independent and dependent feature
 
-            input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
-            target_feature_test_df = test_df[target_column_name]
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
 
-            logging.info("Applying preprocessing on training and testing dataframe")
+            logging.info("Applying Preprocessing on training and test dataframe")
 
-            input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr = preprocessor_obj.transform(input_feature_test_df)
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_features_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
 
             train_arr = np.c_[
-                input_feature_train_arr,np.array(target_feature_train_df)
+                input_feature_train_arr, np.array(target_feature_train_df)
             ]
-            test_arr = np.c_[
-                input_feature_test_arr,np.array(target_feature_test_df)
-            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
-            logging.info(f"saved preprocessing object")
+            logging.info(f"Saved preprocessing object")
 
             save_object(
+
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
-                obj=preprocessor_obj
+                obj=preprocessing_obj
             )
-            return(
+
+            return (
+
                 train_arr,
                 test_arr,
                 self.data_transformation_config.preprocessor_obj_file_path
@@ -113,6 +114,11 @@ class DataTransformation:
 
 
 
-        
+
+
+
+
+
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(sys,e)
+
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -4,6 +4,8 @@ from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 import pymysql
 
 import pickle
@@ -14,21 +16,21 @@ load_dotenv()
 host=os.getenv("host")
 user=os.getenv("user")
 password=os.getenv("password")
-db=os.getenv("db")
+db=os.getenv('db')
 
 
 
 def read_sql_data():
-    logging.info("Reading sql database started")
+    logging.info("Reading SQL database started")
     try:
-        mydb = pymysql.connect(
+        mydb=pymysql.connect(
             host=host,
             user=user,
             password=password,
             db=db
         )
         logging.info("Connection Established",mydb)
-        df = pd.read_sql_query('select * from students',mydb)
+        df=pd.read_sql_query('Select * from students',mydb)
         print(df.head())
 
         return df
@@ -38,14 +40,46 @@ def read_sql_data():
     except Exception as e:
         raise CustomException(e,sys)
     
-def save_object(file_path,obj):
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
 
-        os.makedirs(dir_path,exist_ok=True)
+        os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys) 
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train,X_test,y_test,models,params):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+     
